cc75138e5c060d846ef047685da24a8dcac26f49
dengol04 <dengol04@mail.ru>
2024-10-03 09:43:39 +0300

    result

--- task/log_muncher.rb
+++ task/log_muncher.rb
@@ -1,2 +1,30 @@
-include Gem::Text
-# levenshtein_distance('asd', 'sdf') # => 2
+require "levenshtein"
+
+def get_list_of_contestant
+  file_path = 'log.txt'
+  hash = Hash.new(0)
+  File.foreach(file_path) do |line|
+    match = line.match(/vote => (.+)/)
+    contestant = match[1].chomp
+    count = 0
+    hash.each do |k, v|
+      if hash.include?(contestant)
+        hash[contestant] += 1
+        break
+      end
+      if Levenshtein.distance(k, contestant) < 5
+        hash[k] += 1
+        break
+      end
+      count += 1
+    end
+    if count == hash.size()
+      hash[contestant] = 1
+    end
+  end
+
+  hash
+end
+
+puts get_list_of_contestant.max_by{|k,v| v}
+#puts levenshtein_distance("павпв", "пвапжззз")
